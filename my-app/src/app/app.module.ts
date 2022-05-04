30903359b826b1f6059e026f4993e16aac1a2933
Enzo Vargas <enzovargazzz65@gmail.com>
2022-05-03 22:34:07 -0500

    finished with navigations

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -6,18 +6,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    ProductAlertsComponent
+    ProductAlertsComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
+      {path:'products/:productId', component: ProductDetailsComponent},
     ])
   ],
   providers: [],
